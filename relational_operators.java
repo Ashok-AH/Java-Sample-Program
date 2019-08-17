b0d688dd7414f1109cef95fe9795e7ad38b12546
Ashok-AH <54201430+Ashok-AH@users.noreply.github.com>
2019-08-17 13:15:52 +0530

    Add files via upload
    this is relational operators program

---
+++ relational_operators.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class relational_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20, b = 15;
+		System.out.println(a << 2);
+		System.out.println(b >> 4);
+		System.out.println(a >> 2);
+		System.out.println(b << 4);
+
+	}
+
+}
+
+
